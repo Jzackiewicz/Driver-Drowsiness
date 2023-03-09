2020cd2de962fe71a65f53db3adf230b688e0a0c
Jakzac543 <kubazac4@gmail.com>
2023-03-09 02:07:51 +0100

    final

---
+++ database.py
@@ -0,0 +1,123 @@
+import cv2
+
+
+class Indices:
+    FACE_OVAL = [10, 338, 297, 332, 284, 251, 389, 356, 454, 323, 361, 288, 397, 365, 379, 378, 400, 377, 152, 148, 176,
+                 149, 150, 136, 172, 58, 132, 93, 234, 127, 162, 21, 54, 103, 67, 109]
+
+    LIPS = [61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291, 308, 324, 318, 402, 317, 14, 87, 178, 88, 95, 185, 40,
+            39,
+            37, 0, 267, 269, 270, 409, 415, 310, 311, 312, 13, 82, 81, 42, 183, 78]
+    LOWER_LIP = [61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291, 308, 324, 318, 402, 317, 14, 87, 178, 88, 95]
+    UPPER_LIP = [185, 40, 39, 37, 0, 267, 269, 270, 409, 415, 310, 311, 312, 13, 82, 81, 42, 183, 78]
+
+    LEFT_EYE = [362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385, 384, 398, 282,
+                348]  # ostatnie dwa landmarki to brew i polik
+    LEFT_EYEBROW = [336, 296, 334, 293, 300, 276, 283, 282, 295, 285]
+    LEFT_IRIS = [474, 475, 476, 477]
+
+    RIGHT_EYE = [33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161, 246, 52,
+                 119]  # ostatnie dwa landmarki to brew i polik
+    RIGHT_EYEBROW = [70, 63, 105, 66, 107, 55, 65, 52, 53, 46]
+    RIGHT_IRIS = [469, 470, 471, 472]
+
+    # Holistic_pose
+    HOL_LEFT_EYE = [1, 2, 3]
+    HOL_RIGHT_EYE = [4, 5, 6]
+    LEFT_SHOULDER = [11]
+    RIGHT_SHOULDER = [12]
+    LEFT_HAND = [15, 17, 19, 21]
+    RIGHT_HAND = [16, 18, 20, 22]
+    MOUTH = [9, 10]
+
+    # Holistic_hands
+    WRIST = [0]
+    THUMB = [1, 2, 3, 4]
+    INDEX = [5, 6, 7, 8]
+    MIDDLE = [9, 10, 11, 12]
+    RING = [13, 14, 15, 16]
+    PINKY = [17, 18, 19, 20]
+
+
+class Factors:
+    EYES_RATIO_FACTOR = 44 #0.25
+    LIPS_RATIO_FACTOR = 0.55
+    HAND_FACE_DISTANCE_FACTOR = 2.4
+    OPENED_MOUTH_FRAME = 10
+
+    PUPIL_LEFT_THRESHOLD = -5.5
+    PUPIL_RIGHT_THRESHOLD = 7.5
+    PUPIL_UP_THRESHOLD = 4
+    PUPIL_DOWN_THRESHOLD = 0.6
+
+    HEAD_LEFT_THRESHOLD = -0.5
+    HEAD_RIGHT_THRESHOLD = -8.5
+    HEAD_UP_THRESHOLD = 7.8
+    HEAD_DOWN_THRESHOLD = 4.4
+
+    # Keys control
+    CONF_MODE = False
+    SET_THRESHOLD = False
+    SET_THRESHOLD_UP = False
+    SET_THRESHOLD_DOWN = False
+    SET_THRESHOLD_LEFT = False
+    SET_THRESHOLD_RIGHT = False
+
+    AVERAGING_FACTOR = 30
+    OPTIMIZATION_FACTOR = 1
+
+    FONTS = cv2.FONT_HERSHEY_COMPLEX
+
+
+class Counters:
+    CLOSED_EYES_COUNTER = 0
+    OPENED_MOUTH_COUNTER = 0
+    HAND_FACE_FRAME_COUNTER = 0
+    TOTAL_BLINKS = 0
+    TOTAL_CLOSINGS = 0
+
+
+class Outcomes:
+    IS_FACE_DETECTED = False
+    IS_POSE_DETECTED = False
+
+    EYES_RATIO = []
+    ARE_EYES_CLOSED = False
+
+    MOUTH_RATIO = []
+    IS_MOUTH_OPEN = False
+    IS_YAWNING = False
+
+    HANDS_FACE_RATIO = []
+    HAND_FACE_RELATION = ''
+    ARE_HANDS_CLOSE = False
+
+    PUPILS_DIRECTION_RATIO = []
+    LOOKING_DIRECTION = ''
+
+    HEAD_POSITION_ANGLES = []
+    HEAD_POSITION = ''
+
+    SHOW_INDICATORS = False
+    ON_SCREEN = []
+
+
+KEYS = {
+    49: "add_remove(Outcomes.ON_SCREEN,'face mesh')",  # key '1'
+    50: "add_remove(Outcomes.ON_SCREEN,'eyes')",  # key '2'
+    51: "add_remove(Outcomes.ON_SCREEN,'pupils')",  # key '3'
+    52: "add_remove(Outcomes.ON_SCREEN,'lips')",  # key '4'
+    53: "add_remove(Outcomes.ON_SCREEN,'head')",  # key '5'
+    54: "add_remove(Outcomes.ON_SCREEN,'pose mesh')",  # key '6'
+    55: "add_remove(Outcomes.ON_SCREEN,'hands')",  # key '7'
+    56: "cv2.imwrite('wynik2.jpg', frame)",  # key '8'
+    99: "Factors.CONF_MODE = not Factors.CONF_MODE", # key 'c'
+    8: "Outcomes.ON_SCREEN = []", # key 'Backspace'
+    115: "write_thresholds_2file()", # key 's'
+    # 32: "Factors.SET_THRESHOLD = True",
+    # 117: "Factors.SET_THRESHOLD_UP = True",  # u UP
+    # 100: "Factors.SET_THRESHOLD_DOWN = True",  # d DOWN
+    # 108: "Factors.SET_THRESHOLD_LEFT = True",  # l LEFT
+    # 114: "Factors.SET_THRESHOLD_RIGHT = True",  # r RIGHT
+    27: "sys.exit()"  # key 'esc'
+}
---
+++ pose.py
@@ -0,0 +1,120 @@
+import cv2
+import mediapipe as mp
+
+import utils
+from base import MediaPipeAnalysing
+from database import Indices, Factors, Outcomes
+
+
+class PoseAnalysing(MediaPipeAnalysing):
+    def __init__(self):
+        super().__init__('Pose')
+        self.solution = 'Pose'
+        self.hands_face_ratio = None
+
+    def estimate_hand_face_distance_ratios(self, draw=False):
+        left_hand_indices = Indices.LEFT_HAND
+        right_hand_indices = Indices.RIGHT_HAND
+
+        coords = [(int(point.x * self.frame_width), int(point.y * self.frame_height))
+                  for point in
+                  self.results.pose_landmarks.landmark]
+
+        left_hand_point = self.get_average_point(left_hand_indices)
+        right_hand_point = self.get_average_point(right_hand_indices)
+
+        left_ear_point = coords[7]  # Left ear
+        right_ear_point = coords[8]  # Right ear
+        mouth_point = self.get_average_point([Indices.MOUTH[0], Indices.MOUTH[1]])
+        nose_point = self.get_average_point([0])
+
+        left_ear2nose = self.get_euclidean_distance(left_ear_point, nose_point)
+        right_ear2nose = self.get_euclidean_distance(right_ear_point, nose_point)
+        ear2nose = (left_ear2nose + right_ear2nose) / 2
+
+        ll_distance = self.get_euclidean_distance(left_hand_point, left_ear_point)
+        rr_distance = self.get_euclidean_distance(right_hand_point, right_ear_point)
+        lr_distance = self.get_euclidean_distance(left_hand_point, right_ear_point)
+        rl_distance = self.get_euclidean_distance(right_hand_point, left_ear_point)
+
+        lm_distance = self.get_euclidean_distance(left_hand_point, mouth_point)
+        rm_distance = self.get_euclidean_distance(right_hand_point, mouth_point)
+
+        hand_face_distance_ratios = [ll_distance / ear2nose, lr_distance / ear2nose, rr_distance / ear2nose,
+                                     rl_distance / ear2nose]  # lm_distance / ear2nose, rm_distance / ear2nose]
+
+        if draw:
+            cv2.circle(self.img, left_hand_point[:2], 4, utils.YELLOW, 8)
+            cv2.circle(self.img, right_hand_point[:2], 4, utils.ORANGE, -1)
+            cv2.circle(self.img, left_ear_point[:2], 4, utils.MAGENTA, -1)
+            cv2.circle(self.img, right_ear_point[:2], 4, utils.PINK, -1)
+            cv2.circle(self.img, mouth_point[:2], 4, utils.RED, -1)
+            cv2.circle(self.img, nose_point[:2], 4, utils.RED, -1)
+
+            cv2.line(self.img, left_hand_point[:2], left_ear_point[:2], utils.YELLOW, 2)
+            cv2.line(self.img, right_hand_point[:2], right_ear_point[:2], utils.ORANGE, 2)
+            cv2.line(self.img, left_hand_point[:2], right_ear_point[:2], utils.MAGENTA, 2)
+            cv2.line(self.img, right_hand_point[:2], left_ear_point[:2], utils.PINK, 2)
+            cv2.line(self.img, right_hand_point[:2], mouth_point[:2], utils.GRAY, 2)
+            cv2.line(self.img, left_hand_point[:2], mouth_point[:2], utils.GRAY, 2)
+
+        return hand_face_distance_ratios
+
+    def get_hand_face_position(self):
+        ratios = self.hands_face_ratio
+        if min(ratios[:4]) < Factors.HAND_FACE_DISTANCE_FACTOR:
+            hand_face = "Hand is too close to the ear"
+            Outcomes.ARE_HANDS_CLOSE = True
+        else:
+            hand_face = "It's okay"
+            Outcomes.ARE_HANDS_CLOSE = False
+
+        return hand_face
+
+    def get_all_ratios(self):
+        Outcomes.HANDS_FACE_RATIO.append(self.estimate_hand_face_distance_ratios())
+
+    def get_all_outcomes(self):
+        hand_ratios = Outcomes.HANDS_FACE_RATIO
+        self.hands_face_ratio = list(map(lambda *x: sum(x) / len(hand_ratios), *hand_ratios))
+        Outcomes.HAND_FACE_RELATION = self.get_hand_face_position()
+        self.hands_face_ratio = [round(x, 1) for x in self.hands_face_ratio]
+        Outcomes.HANDS_FACE_RATIO = []
+        Outcomes.HANDS_FACE_RATIO.append(self.hands_face_ratio)
+
+    def draw_indicators(self, objects, frame):
+        height = 150
+        f_width = round(100 / 2100 * self.frame_width)
+        f_height = round(1000 / 1181 * self.frame_height)
+        font_scale = self.frame_width / 2100 * 1.5
+
+        if 'pose mesh' in objects:
+            mp_drawing = mp.solutions.drawing_utils
+            mp_pose = mp.solutions.pose
+            draw_spec = mp_drawing.DrawingSpec(thickness=3, circle_radius=1,
+                                               color=(150, 0, 0))  # color=(100, 100, 100))
+
+            if self.results.pose_landmarks:
+                mp_drawing.draw_landmarks(frame, self.results.pose_landmarks, mp_pose.POSE_CONNECTIONS, draw_spec,
+                                          draw_spec)
+
+        if 'hands' in objects:
+            height -= 150
+            self.estimate_hand_face_distance_ratios(True)
+
+            if Outcomes.HAND_FACE_RELATION == "Hand is too close to the ear":
+                color = utils.RED
+                pos = 'TooClose|'
+            else:
+                pos = ''
+                color = utils.WHITE
+
+            utils.colorBackgroundText(frame, f'Hand-Face dist.: {pos}{self.hands_face_ratio[0]}|'
+                                             f'{self.hands_face_ratio[1]}|{self.hands_face_ratio[2]}|'
+                                             f'{self.hands_face_ratio[3]}',
+                                      Factors.FONTS, font_scale, (f_width, f_height + height), 2, utils.BLACK, color)
+
+            utils.colorBackgroundText(frame, f'Threshold: {Factors.HAND_FACE_DISTANCE_FACTOR}', Factors.FONTS,
+                                      font_scale, (f_width, f_height + 50 + height), 2, utils.BLACK, utils.WHITE)
+
+        return frame
